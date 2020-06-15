17050bb21f99f0280122111664bd99b2102c60ac
Charlie Martin <cdhm1@yahoo.com>
2020-06-14 18:35:04 -0700

    Lab 1.02

--- cpp-course/cpp-course.cpp
+++ cpp-course/cpp-course.cpp
@@ -1,12 +1,56 @@
-// cpp-course.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// C++ Course
+//Lab 1.02
 
 #include <iostream>
-
+#include <math.h>
 #include "Lab_1_02.h"
 
 int main()
 {
 	Lab_1_02 lab_1_02;
 	lab_1_02.execute();
+
+	int a = 5 + 2 * 2;
+	cout << a << endl;
+
+	float b = 2 / 3;
+	cout << b << endl;
+
+	double c = 2.0 * 1.5;
+	cout << c << endl;
+
+	unsigned int d = (2 + 3) * 10;
+	cout << d << endl;
+
+	double e = floor(5.0 / 2);
+	cout << e << endl;
+
+	double e2 = ceil(5 / 2);
+	cout << e2 << endl;
+
+	int e3 = 5 / 2;
+	cout << e3 << endl;
+
+	float f = 5 % 2;
+	cout << f << endl;
+
+	char g = 'g';
+	cout << g << endl;
+
+	char g2 = -127;
+	cout << g2 << endl;
+
+	unsigned char g3 = -127;
+	cout << g3 << endl;
+
+	bool h = true;
+	cout << h << endl;
+
+	bool i = 0;
+	cout << i << endl;
+
+	bool j = -100;
+	cout << j << endl;
+
+
 }
