5f40f9d5668837ddc7425f2c5eec7d2be4ed025e
Charlie Martin <cdhm1@yahoo.com>
2020-06-15 16:44:00 -0700

    Completed Part 1 and Part 2 of Lab 1.02

--- cpp-course/Lab_1_02.cpp
+++ cpp-course/Lab_1_02.cpp
@@ -1,10 +1,107 @@
 #include "Lab_1_02.h"
 
+
 void Lab_1_02::execute()
 {
 	cout << "Executing Lab 1.02" << endl;
 
-	//** code goes here
+	//Part 1
+	int a = 5 + 2 * 2;
+	cout << a << endl;
+
+	float b = 2 / 3;
+	cout << b << endl;
+
+	double c = 2.0 * 1.5;
+	cout << c << endl;
+
+	unsigned int d = (2 + 3) * 10;
+	cout << d << endl;
+
+	double e = floor(5.0 / 2);
+	cout << e << endl;
+
+	double e2 = ceil(5 / 2);
+	cout << e2 << endl;
+
+	int e3 = 5 / 2;
+	cout << e3 << endl;
+
+	float f = 5 % 2;
+	cout << f << endl;
+
+	char g = 'g';
+	cout << g << endl;
+
+	char g2 = -127;
+	cout << g2 << endl;
+
+	unsigned char g3 = -127;
+	cout << g3 << endl;
+
+	bool h = true;
+	cout << h << endl;
+
+	bool i = 0;
+	cout << i << endl;
+
+	bool j = -100;
+	cout << j << endl;
+
+	//Part 2
+
+	int A2 = 10 * 2;
+	cout << A2 << endl;
+
+	int B2 = .5 * 2;
+	cout << B2 << endl;
+
+	int C2 = 10 / 2;
+	cout << C2 << endl;
+
+	int D2 = 10 % 2;
+	cout << D2 << endl;
+
+	//Gets an error
+	//float E2 = 2 * *3;
+	//cout << E2 << endl;
+
+	int F2 = (2 + 5) * 3;
+	cout << F2 << endl;
+
+	int G2 = 2 + 5 * 3;
+	cout << G2 << endl;
+	
+	//error
+	//float H2 = "ab" + "12" + "3";
+	//cout << H2 << endl;
+
+	//Error, shoud it be "x"?
+	//string I2 = x;
+	//cout << I2 << endl;
+
+	//error
+	//float J2 = "ab" + "cd";
+	//cout << J2 << endl;
+
+	//error
+	//int K2 = "abc" * 2; 
+	//cout << K2 << endl;
+
+	float L2 = '1' * 2 + '2' * 3;
+	cout << L2 << endl;
+
+	float M2 = 1 * 2 + '2' * 3;
+	cout << M2 << endl;
+
+	//error
+	//float N2 = 'A' * *2;
+	//cout << N2 << endl;
+
+	//error
+	//float O2 = "bc" % 2;
+	//cout << O2 << endl;
+
 
 	// Bonus: type inference in c++!
 	// Leave this code in place and let it execute. We'll talk it later..
--- cpp-course/cpp-course.cpp
+++ cpp-course/cpp-course.cpp
@@ -1,5 +1,5 @@
 // C++ Course
-//Lab 1.02
+
 
 #include <iostream>
 #include <math.h>
@@ -9,48 +9,4 @@ int main()
 {
 	Lab_1_02 lab_1_02;
 	lab_1_02.execute();
-
-	int a = 5 + 2 * 2;
-	cout << a << endl;
-
-	float b = 2 / 3;
-	cout << b << endl;
-
-	double c = 2.0 * 1.5;
-	cout << c << endl;
-
-	unsigned int d = (2 + 3) * 10;
-	cout << d << endl;
-
-	double e = floor(5.0 / 2);
-	cout << e << endl;
-
-	double e2 = ceil(5 / 2);
-	cout << e2 << endl;
-
-	int e3 = 5 / 2;
-	cout << e3 << endl;
-
-	float f = 5 % 2;
-	cout << f << endl;
-
-	char g = 'g';
-	cout << g << endl;
-
-	char g2 = -127;
-	cout << g2 << endl;
-
-	unsigned char g3 = -127;
-	cout << g3 << endl;
-
-	bool h = true;
-	cout << h << endl;
-
-	bool i = 0;
-	cout << i << endl;
-
-	bool j = -100;
-	cout << j << endl;
-
-
 }
