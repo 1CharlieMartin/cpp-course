202b77e1465a27621fb11666ae0821dc939bd572
AusreisserSF <45668270+AusreisserSF@users.noreply.github.com>
2020-06-21 14:08:01 -0700

    Sample code and problem statement for Lab 1.04

---
+++ cpp-course/Lab_1_04.cpp
@@ -0,0 +1,48 @@
+#include "Lab_1_04.h"
+
+
+void Lab_1_04::execute()
+{
+	cout << "Executing Lab 1.04" << endl;
+
+	float x;
+	cout << "Please enter a float value ";
+	cin >> x;
+	cout << "You entered " << x << endl;
+
+	string z;
+	cout << "Please enter a string with embedded spaces ";
+	cin >> z;
+	cout << "You entered " << z << endl;
+
+	cin.clear();
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	cout << "Please try again" << endl;
+	getline(cin, z);
+	cout << "You entered " << z << endl;
+
+	cout << "What would you like to wish for ? " << endl;
+
+	const string ask("What would you like to wish for ? "); // const means that you may not change this string
+	// this will not compile ask[0] = 'X';
+
+	string my("My ");
+	string dog("dog ");
+	string has("has ");
+	string fleas("fleas ");
+	cout << my << dog << has << fleas << endl;
+
+	string final = my + dog + has + fleas; // string may not be const here
+	cout << "final " << final << endl;
+
+	// The next example uses the "append” member function of the string class:
+	string final2; // string may not be const here
+	final2.append(my);
+	final2.append(dog);
+	final2.append(has);
+	final2.append(fleas);
+
+	cout << "final2 " << final2 << endl;
+
+	//** Codes goes here
+}
--- cpp-course/cpp-course.cpp
+++ cpp-course/cpp-course.cpp
@@ -5,12 +5,17 @@
 #include <math.h>
 #include "Lab_1_02.h"
 #include "Lab_1_03.h"
+#include "Lab_1_04.h"
 
 int main()
 {
 	//Lab_1_02 lab_1_02;
 	//lab_1_02.execute();
 
-	Lab_1_03 lab_1_03;
-	lab_1_03.execute();
+	//Lab_1_03 lab_1_03;
+	//lab_1_03.execute();
+
+	Lab_1_04 lab_1_04;
+	lab_1_04.execute();
+
 }
