91bd7741cd8fe25c4829e6f6be63f8674501885c
AusreisserSF <45668270+AusreisserSF@users.noreply.github.com>
2020-06-27 19:48:14 -0700

    Put notes and corrections in Lab_1_05.cpp

--- cpp-course/Lab_1_05.cpp
+++ cpp-course/Lab_1_05.cpp
@@ -13,23 +13,29 @@ void Lab_1_05::exercise_1()
 {
 	cout << "Executing Lab 1.05, exercise_1" << endl;
 	int height;
-	const float INCHES_PER_FOOT = 12.0f;
+	//const float INCHES_PER_FOOT = 12.0f;
+	const int INCHES_PER_FOOT = 12;
 
 	cout << "Please enter your height in integer inches: ";
 	cin >> height;
 
 	//cin_fail is undefined
+	//## Since cin is a class and fail() is a function, the correct syntax is
+	// cin.fail()
+	//## You can also try google: c++ cin fail and you'll see lots of examples
 	//if (cin_fail())
-	//{
-	//	cout << "Sorry, your input was in the wrong format" << endl;
-	//	exit(1);
-	//}
+	if (cin.fail())
+	{
+		cout << "Sorry, your input was in the wrong format" << endl;
+		exit(1);
+	}
+
+	//## Please fix and test all uses of cin
 
 	int heightFeet = height / INCHES_PER_FOOT;
-	//int heightInches = height % INCHES_PER_FOOT;      ---->> gets error
-	int heightInches = height - (heightFeet * INCHES_PER_FOOT);
+	int heightInches = height % INCHES_PER_FOOT; //## constant must be an integer
+	//##int heightInches = height - (heightFeet * INCHES_PER_FOOT);
 	cout << "You are " << heightFeet << " feet and " << heightInches << " inches tall." << endl;
-
 
 }
 void Lab_1_05::exercise_2()
@@ -52,6 +58,7 @@ void Lab_1_05::exercise_2()
 
 	int fullHeightInches = (feetOfHeight * 12) + inchesOfHeight;
 	float heightMeters = fullHeightInches * INCHES_TO_METERS;
+	//## The instructions say to divide weight in pounds by the number of pounds in a kilo
 	float weightInKilos = weightInPounds * POUNDS_TO_KILOS;
 	float BMI = weightInKilos / (pow(heightMeters, 2));
 
