eba9b9c0b94aa0dd43808f1b97c412538bf076e0
Charlie Martin <cdhm1@yahoo.com>
2020-07-02 16:18:54 -0600

    Project 1

--- cpp-course/Project_1.cpp
+++ cpp-course/Project_1.cpp
@@ -2,7 +2,33 @@
 
 void Project_1::execute()
 {
+
+	int userNum;
+
 	cout << "Executing Project 1" << endl;
 
-	//** Code goes here
+	cout << " Welcome to Magic Square" << endl;
+	cout << "Enter a number from 21 to 65: ";
+	cin >> userNum;
+	if (cin.fail())
+	{
+		cout << "Sorry, your input was in the wrong format." << endl;
+		exit(1);
+	}
+
+	if (userNum < 21 || userNum>65)
+	{
+		cout << "Sorry, the number that you entered is not within 21 and 65." << endl;
+		exit(1);
+	}
+
+
+	cout << "You have entered " << userNum << ".";
+
+	cout << "Your magic square is:" << endl;
+	cout << endl;
+	cout << userNum - 20 << " 1 12 7" << endl;
+	cout << "11 8 " << userNum - 21 << " 2" << endl;
+	cout << "5 10 3 " << userNum - 18 << endl;
+	cout << "4 " << userNum - 19 << " 6 9" << endl;
 }
