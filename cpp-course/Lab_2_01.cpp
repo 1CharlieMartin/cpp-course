33acce72715764dcd43a1bb5799d0da883064f22
Charlie Martin <cdhm1@yahoo.com>
2020-07-06 13:00:29 -0600

    Lab 2.01

--- cpp-course/Lab_2_01.cpp
+++ cpp-course/Lab_2_01.cpp
@@ -33,15 +33,31 @@ void Lab_2_01::execute()
 	//** Lab 2.01
 	// Convert the code below to use a copy of the starter code to validate
 	// the input. Do not call exit(1) if the user enters invalid input.
+
 	int x = 0;
 	float y = 0.0f;
-	cout << "Please enter an integer and a float ";
-	cin >> x >> y;
 
-	if (cin.fail())
+
+	while (true) // i.e. an infinite loop
 	{
-		cout << "Sorry, your input was in the wrong format." << endl;
-		exit(1);
+		cout << "Please enter an integer and a float " << endl;
+		cin >> x >> y;
+		
+
+		if (!cin.fail()) {
+			if (cin.peek() == '\n') // at eol?
+				break; // yes, exit loop
+			else
+				cout << "Sorry, you have extraneous input, please try again" << endl;
+		}
+		else {
+			// cin.fail() is true
+			cin.clear(); // clear the fail() condition
+			cout << "You have entered an invalid integer or float, please try again" << endl;
+		}
+
+		cin.ignore(numeric_limits<streamsize>::max(), '\n'); // flushes input up to eol
 	}
 
+	cout << x << " " << y;
 }
