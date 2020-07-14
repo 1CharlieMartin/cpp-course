9d69513ae80a7a1f837e64a295897034d10a97ab
Charlie Martin <cdhm1@yahoo.com>
2020-07-13 18:54:00 -0600

    Lab 2.03

--- cpp-course/Lab_2_03.cpp
+++ cpp-course/Lab_2_03.cpp
@@ -5,6 +5,34 @@ void Lab_2_03::execute()
 {
 	cout << "Executing Lab 2.03" << endl;
 
-//** Code goes here
+	string triType;
+	float a;
+	float b;
+	float c;
+	bool validTri = false;
+
+	cout << "Please enter the three side lengths of your triangle:";
+	cin >> a >> b >> c;
+
+	if ((b + c < a) || (a + b < c) || (c + a < b)) {
+
+		cout << "Your triangle is not valid." << endl;
+		exit(0);
+	}
+
+
+	if (a == b == c) {
+		triType = "equilateral";
+	}
+
+	else if ((a != b) && (a != c) && (b != a) && (b != c) && (c != a) && (c != b)) {
+		triType = "scalene";
+	}
+
+	else if ((a == b) || (a == c) || (b == c)) {
+		triType = "isosceles";
+	}
+
+	cout << "Your triangle is " << triType << " and its perimeter is " << a + b + c << "." << endl;
 
 }
