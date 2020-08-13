6c9b018a475ca7d94265a5dfba550ee03418c7b6
Charlie Martin <cdhm1@yahoo.com>
2020-08-13 15:53:01 -0700

    Lab 2.06

--- cpp-course/Lab_2_06.cpp
+++ cpp-course/Lab_2_06.cpp
@@ -25,44 +25,51 @@ void Lab_2_06::execute()
 
 	while (p < 5) // i.e. an infinite loop
 	{
-		cout << "Enter the a number from 1-9 to pick the square that you would like to place an X: ";
-		cin >> userChoiceX;
-		if (!cin.fail()) {
-			if ((userChoiceX > 9) || (userChoiceX < 1))
-				cout << "Your number is out of bounds. Please try again." << endl;
-			else if (board[userChoiceX] != SPOT_OPEN)
-				cout << "That spot is not open. Please try again." << endl;
+		while (true){
+			cout << "Enter the a number from 1-9 to pick the square that you would like to place an X: ";
+			cin >> userChoiceX;
+			if (!cin.fail()) {
+				if ((userChoiceX > 9) || (userChoiceX < 1))
+					cout << "Your number is out of bounds. Please try again." << endl;
+				else if (board[userChoiceX] != SPOT_OPEN)
+					cout << "That spot is not open. Please try again." << endl;
+				else
+					break;
 
-		}
-		else {
-			// cin.fail() is true
-			cin.clear(); // clear the fail() condition
-			cout << "You have entered an invalid integer, please try again" << endl;
-		}
-
-		cout << "Enter the a number from 1-9 to pick the square that you would like to place an O: ";
-		cin >> userChoiceO;
-		if (!cin.fail()) {
-			if ((userChoiceO > 9) || (userChoiceO < 1))
-				cout << "Your number is out of bounds. Please try again." << endl;
-			else if (board[userChoiceO] != SPOT_OPEN)
-				cout << "That spot is not open. Please try again." << endl;
-
-		}
-		else {
-			// cin.fail() is true
-			cin.clear(); // clear the fail() condition
-			cout << "You have entered an invalid integer, please try again" << endl;
+			}
+			else {
+				// cin.fail() is true
+				cin.clear(); // clear the fail() condition
+				cout << "You have entered an invalid integer, please try again" << endl;
+			}
+			cin.ignore(numeric_limits<streamsize>::max(), '\n'); // flushes input up to eol
 		}
 
-		cin.ignore(numeric_limits<streamsize>::max(), '\n'); // flushes input up to eol
+		while (true) {
+			cout << "Enter the a number from 1-9 to pick the square that you would like to place an O: ";
+			cin >> userChoiceO;
+			if (!cin.fail()) {
+				if ((userChoiceO > 9) || (userChoiceO < 1))
+					cout << "Your number is out of bounds. Please try again." << endl;
+				else if (board[userChoiceO] != SPOT_OPEN)
+					cout << "That spot is not open. Please try again." << endl;
+
+			}
+			else {
+				// cin.fail() is true
+				cin.clear(); // clear the fail() condition
+				cout << "You have entered an invalid integer, please try again" << endl;
+			}
+			cin.ignore(numeric_limits<streamsize>::max(), '\n'); // flushes input up to eol
+		}
+		
 
 		board[(size_t)userChoiceX - 1] = PLAYER_X_SELECTION; // use symbolic constant here; need cast to size_t to get around warning
 		board[(size_t)userChoiceO - 1] = PLAYER_O_SELECTION;
 
 		cout << "------" << endl;
 		for (int i = 0; i < board.size(); i++) {
-			if (board[i] == 0)
+			if (board[i] == SPOT_OPEN)
 				cout << "Z|";
 			if (board[i] == PLAYER_X_SELECTION) // use symbolic constant here
 				cout << "X|";
