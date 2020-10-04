0c30e3ca20f78c07659440120f0be0c48217d929
Charlie Martin <cdhm1@yahoo.com>
2020-10-03 17:41:14 -0700

    Lab 3.03

--- cpp-course/Lab_3_03.cpp
+++ cpp-course/Lab_3_03.cpp
@@ -15,25 +15,86 @@ void Lab_3_03::execute() {
 	Card("Spades", "2", 2), Card("Spades", "3", 3), Card("Spades", "4", 4), Card("Spades", "5", 5), Card("Spades", "6", 6), Card("Spades", "7", 7),
 	Card("Spades", "8", 8), Card("Spades", "9", 9), Card("Spades", "10", 10), Card("Spades", "Jack", 11), Card("Spades", "Queen", 12), Card("Spades", "King", 13), Card("Spades", "Ace", 14)};
 
+	string player1Name;
+	string player2Name;
+	int player1Score = 0;
+	int player2Score = 0;
+	
+	
+
 	// Call the standard library algorithm to shuffle the deck.
 	random_shuffle(deck.begin(), deck.end());
 
 	//** Code goes here
+	cout << "Please enter Player 1's name: " << endl;
+	cin >> player1Name;
+	cout << "Please enter Player 2's name:" << endl;
+	cin >> player2Name;
+	cout << endl;
 
-	//** You can put this at the bottom of your while loop.
-	cout << "Remaining cards in deck " << to_string(deck.size()) << endl;
+	while (!deck.empty()) {
+
+		int p1Value = player_turn(player1Name, deck);
+		int p2Value = player_turn(player2Name, deck);
+
+		if (p1Value > p2Value) {
+			player1Score++;
+			cout << player1Name << " has won this round!" << endl;
+			cout << player1Name << "'s current score is: " << player1Score << endl;
+			cout << player2Name << "'s current score is: " << player2Score << endl;
+		}
+
+		else if (p2Value > p1Value) {
+			player2Score++;
+			cout << player2Name << " has won this round!" << endl;
+			cout << player1Name << "'s current score is: " << player1Score << endl;
+			cout << player2Name << "'s current score is: " << player2Score << endl;
+		}
+
+		else {
+			cout << "Both player's cards have the same value. This is war." << endl;
+		}
+
+		cout << "Remaining cards in deck " << to_string(deck.size()) << endl;
+
+		if ((deck.empty()) && (player1Score > player2Score)) {
+			cout << "THE WINNER OF THE GAME IS " << player1Name << "!!" << endl;
+			cout << player1Name << "'s final score was: " << player1Score << endl;
+			cout << player2Name << "'s final score was: " << player2Score << endl;
+		}
+
+		if ((deck.empty()) && (player2Score > player1Score)) {
+			cout << "THE WINNER OF THE GAME IS " << player2Name << "!!" << endl;
+			cout << player1Name << "'s final score was: " << player1Score << endl;
+			cout << player2Name << "'s final score was: " << player2Score << endl;
+		}
+
+
+		cout << endl;
+	}
 }
 
 
-//** Put the contract here.
+// player_turn
+// Purpose: Remove a card from a randomized deck, print the name of the player
+// who drew the card, print out the suit and name of the card, and return the value of the card
+// Arguments:
+// const reference to a player name (string)
+// non-const reference to a deck of cards
+// Returns: int
+
 int Lab_3_03::player_turn(const string& player_name, vector<Card>& deck) {
 	Card card = deck.back(); // access a card
 	deck.pop_back(); // remove the card from the deck
 
+	string playerSuit = card.getSuit();
+	string playerCard = card.getCard();
+
 	//** Code goes here according to the contract.
+	cout << player_name << "'s card is a " << playerCard << " of " << playerSuit << "." << endl;
+	
 
-	//** Use the "getter" functions to access the private fields in the Card class.
-
+	//** Use the "getter" functions to access the private fields in the Card class.	
 	return card.getValue();
 };
 
